e0f05f4f4270848974428f57bc58709e1991acf0
Savrahul <b419044@iiit-bh.ac.in>
2023-07-03 11:26:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import About from './pages/About';
+import Home from './pages/Home';
+import Project from './pages/Project';
+import TechnicalSkills from './pages/TechnicalSkills';
+import Contact from './pages/Contact';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footers';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const AppRoutes = () => {
+    return(<Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/technicalskills" element={<TechnicalSkills />} />
+      <Route path="/project" element={<Project />} />
+    </Routes>
+    );
+  };
+
+  return(
+    <div className='App'>
+      <Navbar/>
+      <AppRoutes/>
+      <Footer/>
     </div>
   );
 }
-
 export default App;
